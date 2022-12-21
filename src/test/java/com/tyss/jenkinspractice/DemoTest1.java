b113ae7762e270512e64fe6c2e78c76a32843ebf
Anil6317 <DELL@DESKTOP-39IR2MT>
2022-12-21 11:09:15 +0530

    new commit

--- src/test/java/com/tyss/jenkinspractice/DemoTest1.java
+++ src/test/java/com/tyss/jenkinspractice/DemoTest1.java
@@ -8,6 +8,7 @@ public class DemoTest1
      public void test1()
      {
     	 System.out.println("welcome to jenkins");
+    	 System.out.println("i m adding new line for cross checking");
      }
 
 }
