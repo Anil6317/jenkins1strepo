f078e5fbfa658a0855dd866206993c75874add7a
Anil6317 <DELL@DESKTOP-39IR2MT>
2022-12-21 10:17:35 +0530

    jenkins  for messi

---
+++ src/test/java/com/tyss/jenkinspractice/DemoTest1.java
@@ -0,0 +1,13 @@
+package com.tyss.jenkinspractice;
+
+import org.testng.annotations.Test;
+
+public class DemoTest1 
+{
+     @Test(groups="sanity")
+     public void test1()
+     {
+    	 System.out.println("welcome to jenkins");
+     }
+
+}
